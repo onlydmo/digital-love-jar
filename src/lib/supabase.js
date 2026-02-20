44781a6f55699604342abed61abfd86ff362bff6
onlydmo <okoh656@gmail.com>
2026-02-20 17:22:21 +0100

    chore: migrate secrets to environment variables

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://btgpocewlfarfkijpgax.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ0Z3BvY2V3bGZhcmZraWpwZ2F4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA3MzEwNzksImV4cCI6MjA4NjMwNzA3OX0.X1OjXcZwXpHe95c2hR1uSULnD50BWO89aqfl5TemCXY';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
