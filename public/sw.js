e17de7289c7a2c1d7a8d58c73e1ec11d6bfd62dd
onlydmo <okoh656@gmail.com>
2026-02-20 22:30:38 +0100

    fix: kill zombie service worker

--- public/sw.js
+++ public/sw.js
@@ -1,23 +1,25 @@
-const CACHE_NAME = 'love-jar-v1';
-const ASSETS_TO_CACHE = [
-    '/',
-    '/index.html',
-    '/manifest.json',
-    '/icon.png'
-];
+// SERVICE WORKER KILL SWITCH
+// This script will immediately unregister itself and clear all caches to fix the white screen crash.
 
 self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => {
-            return cache.addAll(ASSETS_TO_CACHE);
-        })
-    );
+    self.skipWaiting(); // Force the new service worker to take over immediately
 });
 
-self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request).then((response) => {
-            return response || fetch(event.request);
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    console.log('[SW] Deleting cache:', cacheName);
+                    return caches.delete(cacheName);
+                })
+            );
+        }).then(() => {
+            return self.registration.unregister();
+        }).then(() => {
+            return self.clients.matchAll();
+        }).then((clients) => {
+            clients.forEach(client => client.navigate(client.url));
         })
     );
 });
